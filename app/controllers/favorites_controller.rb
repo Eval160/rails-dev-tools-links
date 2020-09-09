05a7a248addea94e85b3d7997f9d550f65842dec
Valentine Ecrepont <ecrepontvalentine@hotmail.fr>
2020-09-09 15:49:16 +0200

    js render for favorite create and destroy

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,13 +1,22 @@
 class FavoritesController < ApplicationController
   def create
     @resource = Resource.find(params[:resource_id])
-    @favorite = Favorite.new(user: current_user, resource: @resource)
-    redirect_to resources_path if @favorite.save
+    @favorite = Favorite.create!(user: current_user, resource: @resource)
+
+    respond_to do |format|
+      format.html { redirect_to resources_path }
+      format.js
+    end
   end
 
   def destroy
     @favorite = Favorite.find(params[:id])
+    @resource = @favorite.resource
     @favorite.destroy
-    redirect_to resources_path
+
+    respond_to do |format|
+      format.html { redirect_to resources_path }
+      format.js
+    end
   end
 end
--- app/javascript/components/category_filter.js
+++ app/javascript/components/category_filter.js
@@ -1,10 +1,12 @@
 const toggleFilter = () => {
   const filterBtn = document.querySelector(".toogleBtn")
   const filterSection = document.querySelector(".category-filter")
+  if (filterBtn) {
     filterBtn.addEventListener("click", (event) => {
       filterSection.classList.toggle("d-none");
       filterBtn.classList.toggle("d-none");
     })
+  }
 
 }
 
